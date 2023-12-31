9b1fc2ae6d9eabf1c4344cb90f1ba656f1ff119a
Jalil <jalilbengoufa@gmail.com>
2023-12-31 16:39:47 -0500

    add IP white listing

--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,11 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
-export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-	//
-	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-	// MY_QUEUE: Queue;
-}
-
-export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return handleRequest(request);
-	},
-};
-
-// addEventListener('fetch', (event: FetchEvent) => {
-// 	event.respondWith(handleRequest(event.request));
-// });
+export interface Env {}
 
 type FIT_TYPE = 'scale-down' | 'contain' | 'cover' | 'crop' | 'pad';
 type FORMAT_TYPE = 'avif' | 'webp' | 'json' | 'jpeg' | 'png';
 type OUTPUT_TYPE = 'thumbnail' | 'small' | 'medium' | 'large';
 
 const ALLOWED_SOURCE_ORIGINS = ['images.unsplash.com'];
-
-//TEST IMAGE https://images.unsplash.com/photo-1682687218147-9806132dc697
+const ALLOWED_IP_LIST = ['00.00.00.00'];
 
 const OUTPUT_SIZES: { [key in OUTPUT_TYPE]: number } = {
 	thumbnail: 150,
@@ -62,7 +26,23 @@ interface ImageOptions {
 	};
 }
 
+export default {
+	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+		return handleRequest(request);
+	},
+};
+
 async function handleRequest(request: Request): Promise<Response> {
+	const ip = request.headers.get('cf-connecting-ip');
+
+	if (!ip) {
+		return new Response('Missing ip header', { status: 400 });
+	}
+
+	if (!ALLOWED_IP_LIST.includes(ip)) {
+		return new Response('Unauthorized', { status: 400 });
+	}
+
 	const url = new URL(request.url);
 	const options: ImageOptions = { cf: { image: {} } };
 
@@ -97,7 +77,7 @@ async function handleRequest(request: Request): Promise<Response> {
 
 	try {
 		const { hostname } = new URL(imageURL);
-		if (ALLOWED_SOURCE_ORIGINS.includes(hostname)) {
+		if (!ALLOWED_SOURCE_ORIGINS.includes(hostname)) {
 			return new Response('Invalid source image URL', { status: 403 });
 		}
 	} catch (err) {
